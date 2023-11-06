1c8e55f37dfae9443692b1c003d8befabf7b8f49
EduardoArce <a222212688@unison.mx>
2023-11-05 18:30:05 -0700

    Initial commit

---
+++ src/main/java/msp/ConnectionHandler.java
@@ -0,0 +1,94 @@
+package msp;
+
+import msp.UserManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+public class ConnectionHandler implements Runnable {
+
+    public static final String CLASS_NAME = ConnectionHandler.class.getSimpleName();
+    public static final Logger LOGGER = Logger.getLogger(CLASS_NAME);
+
+
+    private UserManager users;
+    private Socket clientSocket = null;
+
+    private BufferedReader input;
+    private PrintWriter output;
+
+
+    public ConnectionHandler(UserManager u, Socket s) {
+        users = u;
+        clientSocket = s;
+
+        try {
+            input = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            output = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            LOGGER.severe(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        String buffer = null ;
+        while (true) {
+            try {
+                buffer = input.readLine();
+            } catch (IOException e) {
+                LOGGER.severe(e.getMessage());
+                e.printStackTrace();
+            }
+            String command = buffer.trim();
+            // CONNECT Juan
+            if( command.startsWith("CONNECT") ) {
+                String userName = command.substring(command.indexOf(' ')  ).trim();
+                System.out.println(userName);
+               boolean isConnected =  users.connect(userName,clientSocket);
+               if( isConnected ) {
+                   output.println("OK");
+               } else {
+                   output.println("FAIL");
+               }
+            }
+
+            //DESCONECTAR PAPI
+            else if (command.startsWith("DISCONNECT")) {
+                String userName = command.substring(command.indexOf(' ')  ).trim();
+                boolean Desonectado =    users.disconnect(userName);
+                if (Desonectado) {
+                    output.println("OK");
+                } else {
+                    output.println("FAIL");
+                }
+            }
+
+            // SEND #<mensaje>@<usuario>
+            if( command.startsWith("SEND") ) {
+                String message = command.substring(command.indexOf('#')+1,
+                        command.indexOf('@') );
+                System.out.println(message);
+                String userName = command.substring(command.indexOf('@')+1 ).trim();
+                System.out.println(userName);
+                users.send(message);
+                //output.println(message);
+            }
+            //LISTA PAAA
+            else if (command.equals("LIST")) {
+                String userList = users.obtenerListaUsuario();
+                output.println(userList);
+            }
+
+        }
+
+
+    }
+}
